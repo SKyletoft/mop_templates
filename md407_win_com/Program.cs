30693ad4e4c0aefd41c47c53e4037ec4d45bb87e
Samuel Kyletoft <samuel@kyletoft.se>
2022-02-09 02:22:31 +0000

    Configurable port

--- md407_win_com/Program.cs
+++ md407_win_com/Program.cs
@@ -29,11 +29,22 @@ class Program {
 	static SerialPort get_and_init_port() {
 		Console.WriteLine("Starting");
 		var potential_ports = SerialPort.GetPortNames();
-		foreach (var port_name in potential_ports) {
-			Console.WriteLine(port_name);
+		// Yes, we're assuming there's at least one port. I get COM1 in my VM with nothing connected, so I'm trusting that for real hardware
+		string port_name = potential_ports[0];
+		if (potential_ports.Length > 1) {
+			Console.WriteLine("Potential ports:");
+			foreach (var pot_port in potential_ports) {
+				Console.WriteLine(pot_port);
+			}
+			Console.Write("> ");
+			port_name = Console.ReadLine() ?? port_name;
+			if (!potential_ports.Contains(port_name)) {
+				Console.Error.WriteLine("Not a valid port name, trying anyway");
+			}
 		}
+		// Some boards use the first value, though seemingly most of them use the second
 		var baud_rates = new int[] {124400, 115200};
-		var port = new SerialPort("COM3", baud_rates[1]);
+		var port = new SerialPort(port_name, baud_rates[1]);
 		port.Open();
 		Console.WriteLine("Connected");
 		return port;
