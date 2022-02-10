8ae6a8c632ffb7f34ca66af046d8690590d7aad4
Samuel Kyletoft <samuel@kyletoft.se>
2022-02-10 21:45:52 +0100

    Refactor to move all the boiler plate out of the main file

---
+++ md407_win_rs/src/error.rs
@@ -0,0 +1,36 @@
+use std::{str::FromStr, fmt::Display};
+
+use crate::Mode;
+
+impl FromStr for Mode {
+	type Err = Error;
+
+	fn from_str(s: &str) -> Result<Self, Self::Err> {
+		let lowercase = s.to_lowercase();
+		let res = match lowercase.as_str() {
+			"load" => Mode::Load,
+			"go" => Mode::Go,
+			"interactive" => Mode::Interactive,
+			_ => return Err(Error),
+		};
+		Ok(res)
+	}
+}
+
+impl Display for Mode {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		write!(f, "{self:?}")
+	}
+}
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub struct Error;
+
+impl Display for Error {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		write!(f, "Error")
+	}
+}
+
+impl std::error::Error for Error {}
+
