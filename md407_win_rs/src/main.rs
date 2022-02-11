ce92567e1579b52fc006c59fcb46cbdfa02a2648
Samuel Kyletoft <samuel@kyletoft.se>
2022-02-11 11:26:18 +0100

    Replace panics with anyhow

--- md407_win_rs/src/main.rs
+++ md407_win_rs/src/main.rs
@@ -6,6 +6,7 @@ use crossterm::{
 	terminal,
 };
 use serialport::SerialPort;
+use anyhow::Result;
 
 #[derive(Parser, Debug, PartialEq, Clone)]
 #[clap(author, about)]
@@ -50,60 +51,63 @@ enum Mode {
 fn main() {
 	let args = Mode::parse();
 
-	match args {
+	if let Err(e) = match args {
 		Mode::Load {
 			port,
 			baud_rate,
 			filename,
-		} => load_mode(connect(&port, baud_rate), &filename),
-		Mode::Go { port, baud_rate } => go_mode(connect(&port, baud_rate)),
-		Mode::Interactive { port, baud_rate } => interactive_mode(connect(&port, baud_rate)),
+		} => connect(&port, baud_rate).and_then(|connection| load_mode(connection, &filename)),
+		Mode::Go { port, baud_rate } => connect(&port, baud_rate).and_then(go_mode),
+		Mode::Interactive { port, baud_rate } => connect(&port, baud_rate).and_then(interactive_mode),
 		Mode::Query => query_mode(),
+	} {
+		eprintln!("Error: {e:?}");
 	}
 }
 
-fn connect(name: &str, baud_rate: u32) -> impl SerialPort {
-	let mut port = serialport::new(name, baud_rate)
-		.open_native()
-		.expect("Failed to open port");
-	port.set_timeout(Duration::from_millis(50))
-		.expect("Couldn't set timeout");
-	port
+fn connect(name: &str, baud_rate: u32) -> Result<impl SerialPort> {
+	let mut port = serialport::new(name, baud_rate).open_native()?;
+	port.set_timeout(Duration::from_millis(50))?;
+	Ok(port)
 }
 
-fn query_mode() {
-	let ports = serialport::available_ports().expect("Failed to query ports");
+fn query_mode() -> Result<()> {
+	let ports = serialport::available_ports()?;
 	for port in ports.iter() {
 		println!("{}", port.port_name);
 	}
+	Ok(())
 }
 
-fn load_mode(mut port: impl SerialPort, filename: &str) {
-	let file = std::fs::read(filename).expect("Could not read file. Does it exist?");
-	port.write_all(b"load\n").expect("Write error");
-	port.write_all(&file).expect("Write error");
+fn load_mode(mut port: impl SerialPort, filename: &str) -> Result<()> {
+	let file = std::fs::read(filename)?;
+	port.write_all(b"load\n")?;
+	port.write_all(&file)?;
 
 	#[cfg(windows)]
-	port.write_all(b"\n").expect("Write error");
+	port.write_all(b"\n")?;
+
+	Ok(())
 }
 
-fn go_mode(mut port: impl SerialPort) {
-	port.write_all(b"go\n").expect("Write error");
+fn go_mode(mut port: impl SerialPort) -> Result<()> {
+	port.write_all(b"go\n")?;
+	Ok(())
 }
 
-fn interactive_mode(mut port: impl SerialPort) {
-	terminal::enable_raw_mode().expect("Couldn't enter raw mode");
+fn interactive_mode(mut port: impl SerialPort) -> Result<()> {
+	terminal::enable_raw_mode()?;
 	let stdout = std::io::stdout();
 	let mut stdout = stdout.lock();
 	let mut small_buffer = b'\0';
 
-	port.write_all(b"\n").expect("Write error");
-	port.flush().expect("Write error");
+	port.write_all(b"\n")?;
+	port.flush()?;
 
 	loop {
 		// Check for input from user
-		if event::poll(Duration::from_millis(1)).expect("input error") {
-			match event::read().expect("input error") {
+		if event::poll(Duration::from_millis(1))? {
+			match event::read()? {
 				Event::Key(KeyEvent {
 					// Lower case on Linux, upper case on Windows
 					code: KeyCode::Char('c' | 'C'),
@@ -116,8 +120,7 @@ fn interactive_mode(mut port: impl SerialPort) {
 						KeyCode::Backspace => 0x08,
 						_ => continue,
 					};
-					port.write_all(slice::from_mut(&mut byte_rep))
-						.expect("Write error");
+					port.write_all(slice::from_mut(&mut byte_rep))?;
 				}
 				_ => {}
 			}
@@ -128,25 +131,24 @@ fn interactive_mode(mut port: impl SerialPort) {
 			// We only read a single byte at a time because the hardware times
 			// out otherwise
 			let _ = port
-				.read(slice::from_mut(&mut small_buffer))
-				.expect("Read error");
+				.read(slice::from_mut(&mut small_buffer))?;
 
 			// Windows can't handle printing 0xFF
 			if small_buffer == u8::MAX {
 				continue;
 			}
 
-			stdout
-				.write_all(slice::from_ref(&small_buffer))
-				.expect("Error writing to stdout");
+			stdout.write_all(slice::from_ref(&small_buffer))?;
 
 			// Because we're in raw mode
 			if small_buffer == b'\n' {
-				stdout.write_all(b"\r").expect("Error writing to stdout");
+				stdout.write_all(b"\r")?;
 			}
-			stdout.flush().expect("Error flushing stdout");
+			stdout.flush()?;
 		}
 	}
-	terminal::disable_raw_mode().expect("Couldn't exit raw mode");
-	writeln!(stdout).expect("Error writing to stdout");
+	terminal::disable_raw_mode()?;
+	writeln!(stdout)?;
+
+	Ok(())
 }
