f2255a7cd76e66b3d73e51cc7990398eb4ed5a41
Samuel Kyletoft <samuel@kyletoft.se>
2022-02-12 20:01:40 +0100

    Start implementing commands

--- vsc_gui/src/extension.ts
+++ vsc_gui/src/extension.ts
@@ -4,19 +4,54 @@ import * as vscode from 'vscode';
 import { PortConfig } from './port_config';
 import { BaudRateConfig } from './baud_rate_config';
 import { Actions } from './actions';
+import { MD407WinRsWrapper } from './native_com';
 
 let port = "";
 let baud_rate = "115'200";
+const md407_win_rs = new MD407WinRsWrapper();
+
+function get_baud_rate() {
+	let filtered = baud_rate.replace("'", "");
+	console.log(baud_rate, filtered);
+	return parseInt(filtered);
+}
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	vscode.commands.registerCommand('md407.run', (entry) => {
+	vscode.commands.registerCommand('md407.run', async (entry) => {
 		switch (entry.label) {
-			case "Compile": break;
-			case "Load": break;
-			case "Go": break;
-			case "Interactive": break;
+			case "Compile": {
+				const tasks = await vscode.tasks.fetchTasks();
+				const build_task = tasks.filter((task) => task.name === 'build')[0];
+				console.log(tasks);
+				await vscode.tasks.executeTask(build_task);
+			} break;
+			case "Load": {
+				if (port === "") {
+					vscode.window.showErrorMessage("You have to set a port");
+					break;
+				}
+				const out = md407_win_rs.load(port, get_baud_rate());
+				vscode.window.showInformationMessage("Load complete");
+				console.log(out);
+			} break;
+			case "Go": {
+				if (port === "") {
+					vscode.window.showErrorMessage("You have to set a port");
+					break;
+				}
+			} break;
+			case "Interactive": {
+				if (port === "") {
+					vscode.window.showErrorMessage("You have to set a port");
+					break;
+				}
+				const NAME = "md407-interactive";
+				const term = vscode.window.terminals.find((t) => t.name === NAME) || vscode.window.createTerminal(NAME);
+				term.show();
+				term.sendText([md407_win_rs.path, "interactive", "--port", port, "--baud-rate", get_baud_rate()].join(" "), true);
+			} break;
 		}
 		console.log([entry.label, port, baud_rate].join(" "));
 	});
