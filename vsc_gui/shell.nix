2db0282f02b12c11f14cc55abbffad4ba4c3fca7
Samuel Kyletoft <samuel@kyletoft.se>
2022-03-24 01:07:54 +0100

    Nix: Attempt to add VSCE

--- vsc_gui/shell.nix
+++ vsc_gui/shell.nix
@@ -1,13 +1,24 @@
 { pkgs ? import <nixpkgs> {} }:
-pkgs.mkShell {
-	# nativeBuildInputs is usually what you want -- tools you need to run
+let
+	# node2nix -i <(echo '["vsce", "esbuild", "yo", "generator-code", "prebuild-install"]') --include-peer-dependencies
+	custom_npm = import ./vsce_nix/default.nix {};
+in pkgs.mkShell {
+	buildInputs = with pkgs; [  ];	
 	nativeBuildInputs = with pkgs; [
+		custom_npm.yo
+		custom_npm.generator-code
+		custom_npm.esbuild
+		custom_npm.prebuild-install
+		custom_npm.vsce
+
+		pkgconfig
+		libsecret.dev
+		libsecret.out
 		nodejs
-		nodePackages.yo
-		nodePackages.generator-code
 		clang-tools # Clang format
 	];
 	shellHook = ''
+		ln -s $NODE_PATH node_modules
 		PS1="\e[32;1mnix-shell: \e[34m\w \[\033[00m\]\n↳ "
 	'';
 }
